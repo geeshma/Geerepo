470f68f70cd06a6e5a6f6928a07ed1b6254e08af
geeshma <59185481+geeshma@users.noreply.github.com>
2021-09-09 21:21:12 +0530

    gp,backup,GitPractice1

---
+++ GitPractice1/testing/GitTestProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestingWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Manas");
+            Console.WriteLine("Kamla");
+            Console.WriteLine("Tiwari");
+            Console.WriteLine("Rahul");
+            Console.WriteLine("Sakshi");
+            Console.WriteLine("Sowmil");
+            Console.WriteLine("Geeshma");
+            Console.WriteLine("Team5");
+        }
+    }
+}
